82bd53b70b9da57c6b1a7d56af313720572b5bf9
Andrew Gazelka <andrew.gazelka@gmail.com>
2019-02-08 17:54:41 -0600

    use hashmap Problem 1
    ~600 ms

--- src/main/kotlin/eulerproject/Problem14.kt
+++ src/main/kotlin/eulerproject/Problem14.kt
@@ -2,21 +2,49 @@ package eulerproject
 
 import kotlin.system.measureTimeMillis
 
-val set = HashSet<Long>()
+val map = HashMap<Long, Int>()
 
-fun main() {
+val to = 1_000_000L
+
+
+fun test1() {
     measureTimeMillis {
-        (1_000_000L downTo 1L).maxBy {
-            if(set.contains(it)) return@maxBy 0
-            else return@maxBy it.chainLength()
-        }.also { println(it) }
-    }.also { println("millis: $it") }
+        (to downTo 1L).maxBy {
+            if (map.containsKey(it)) {
+                return@maxBy 0
+            } else return@maxBy it.chainLength()
+        }.also { println("max: $it") }
+    }.also {
+        println("millis: $it")
+        println(map)
+    }
 }
 
-tailrec fun Long.chainLength(length: Int = 0): Int {
-//    println(this)
-    set.add(this)
-    if(this == 1L) return length
-    if(this % 2L == 0L) return (this/2L).chainLength(length+1)
-    return (3L*this+1).chainLength(length+1)
+fun main() {
+    test1()
+}
+
+fun Long.chainLength(length: Int = 1): Int {
+    if (this == 1L) {
+        return length
+    }
+
+    val mapVal = map[this]
+
+    if (this % 2L == 0L) {
+        return if (mapVal == null) {
+            val res = (this / 2L).chainLength()
+            map[this] = res
+            res + length
+        } else {
+            mapVal + length
+        }
+    }
+    return if (mapVal == null) {
+        val res = (3L * this + 1).chainLength()
+        map[this] = res
+        res + length
+    } else {
+        mapVal + length
+    }
 }
